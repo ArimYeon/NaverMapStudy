69c9c1152db2c391491f0a3b30427f86b657c6fb
arimyeon <dusdkfla@naver.com>
2020-12-17 21:04:00 +0900

    MapView create

--- app/src/main/java/com/arimyeon/testmap/MainActivity.kt
+++ app/src/main/java/com/arimyeon/testmap/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.arimyeon.testmap
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        map_btn.setOnClickListener {
+            val mapIntent = Intent(this, NaverMapActivity::class.java)
+            startActivity(mapIntent)
+        }
+        map_view_btn.setOnClickListener {
+            val mapViewIntent = Intent(this, NaverMapViewActivity::class.java)
+            startActivity(mapViewIntent)
+        }
     }
 }
