a2dadac7a5cc423d0b81a80e2c2e7b3f522b2cfa
arimyeon <dusdkfla@naver.com>
2020-12-17 23:16:01 +0900

    add location tracking

--- app/src/main/java/com/arimyeon/testmap/NaverMapViewActivity.kt
+++ app/src/main/java/com/arimyeon/testmap/NaverMapViewActivity.kt
@@ -2,11 +2,17 @@ package com.arimyeon.testmap
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.naver.maps.map.MapView
+import android.widget.Toast
+import androidx.annotation.UiThread
+import com.naver.maps.map.*
+import com.naver.maps.map.util.FusedLocationSource
 
-class NaverMapViewActivity : AppCompatActivity() {
+class NaverMapViewActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mapView: MapView
+    // 위치를 반환하는 구현체
+    private lateinit var locationSource: FusedLocationSource
+    private lateinit var naverMap: NaverMap
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +21,12 @@ class NaverMapViewActivity : AppCompatActivity() {
 
         mapView = findViewById(R.id.map_view)
         mapView.onCreate(savedInstanceState)
+        // NaverMap 객체 얻어오기
+        // MapView의 getMapAsync() 메서드로 OnMapReadyCallback을 등록하면 비동기로 NaverMap 객체를 얻을 수 있음
+        // NaverMap 객체가 준비되면 onMapReady() 콜백 메서드가 호출됨
+        mapView.getMapAsync(this)
+        // FusedLocationSource는 런타임 권한 처리를 위해 액티비티 또는 프래그먼트를 필요로 함
+        locationSource = FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
     }
 
     override fun onStart() {
@@ -50,5 +62,33 @@ class NaverMapViewActivity : AppCompatActivity() {
     override fun onLowMemory() {
         super.onLowMemory()
         mapView.onLowMemory()
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        if (locationSource.onRequestPermissionsResult(requestCode, permissions, grantResults)) {
+            if (!locationSource.isActivated) { // 권한 거부됨
+                naverMap.locationTrackingMode = LocationTrackingMode.None
+            }
+            return
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    @UiThread
+    override fun onMapReady(naverMap: NaverMap) {
+        this.naverMap = naverMap
+        naverMap.locationSource = locationSource
+        // 위치 추적 모드 활성화
+        naverMap.locationTrackingMode = LocationTrackingMode.Follow
+        // 위치 변경 이벤트
+        // 위치 추적 모드가 활성화되고 사용자의 위치가 변경되면 onLocationChange() 콜백 메서드가 호출되며, 파라미터로 사용자의 위치가 전달됨
+        naverMap.addOnLocationChangeListener { location ->
+            Toast.makeText(this, "${location.latitude}, ${location.longitude}",
+                Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1000
     }
 }
